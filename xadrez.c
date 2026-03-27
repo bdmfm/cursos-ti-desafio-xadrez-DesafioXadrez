0c003eafb6df96707d6ed33c9de2d143e03827dd
bdmfm <202603666069@alunos.estacio.br>
2026-03-27 20:32:47 -0300

    arquivo no main ao inves de estar num branch

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,7 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
+//chato demais esse dev online
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
